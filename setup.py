43afa2e24dc87e85fdd6c567f8f2675ab425b164
Saurabh Dubey <saurabh.dubey@genzeon.com>
2025-01-03 18:46:13 +0530

    added log creation file

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='A smart logger that enhances Python logging.',
     author='Saurabh Dubey',
     author_email='saurabh.dubey@genzeon.com',
-    url='https://github.com/yourusername/smart-logger',  # Replace with your actual GitHub URL
+    url='https://github.com/saurabhkrdubey/smart-logger',  # Replace with your actual GitHub URL
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
--- smart_logger/logger.py
+++ smart_logger/logger.py
@@ -4,11 +4,68 @@ import os
 
 
 
+import logging
+import os
+
+class CustomFormatter(logging.Formatter):
+    def format(self, record):
+        # Adding a custom variable if needed
+        record.custom_variable = "Custom Value"
+        return super().format(record)
+
 class SmartLogger:
-    def __init__(self, name='smart-logger'):
+    def __init__(self, name='smart-logger', log_dir='logs', log_file='app.log'):
+        # Create a log directory if it doesn't exist
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+
         # Create a custom logger
         self.logger = logging.getLogger(name)
         self.logger.setLevel(logging.DEBUG)
+
+        # Create a file handler to write logs to a file
+        log_file_path = os.path.join(log_dir, log_file)
+        file_handler = logging.FileHandler(log_file_path)
+        file_handler.setLevel(logging.DEBUG)
+
+        # Create a console handler
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)
+
+        # Use the CustomFormatter with both handlers
+        formatter = CustomFormatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(funcName)s - %(message)s - [Custom Variable: %(custom_variable)s]'
+        )
+        file_handler.setFormatter(formatter)
+        console_handler.setFormatter(formatter)
+
+        # Add the handlers to the logger
+        self.logger.addHandler(file_handler)
+        self.logger.addHandler(console_handler)
+
+
+
+
+
+class SmartLogger:
+    def __init__(self, name='smart-logger', log_dir='logs'):
+        frame = inspect.currentframe().f_back
+        frame_info = inspect.getframeinfo(frame)
+        filename = os.path.basename(frame_info.filename)
+        self.filename = os.path.splitext(os.path.basename(filename))[0]
+
+        # Create a log directory if it doesn't exist
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+
+
+        # Create a custom logger
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+
+        log_file_path = os.path.join(log_dir,f'{self.filename}_log.log')
+        file_handler = logging.FileHandler(log_file_path)
+        file_handler.setLevel(logging.DEBUG)
 
         # Create console handler
         ch = logging.StreamHandler()
@@ -21,6 +78,12 @@ class SmartLogger:
         ch.setFormatter(formatter)
 
         # Add the handler to the logger
+
+        file_handler.setFormatter(formatter)
+        ch.setFormatter(formatter)
+
+        # Add the handlers to the logger
+        self.logger.addHandler(file_handler)
         self.logger.addHandler(ch)
 
 
